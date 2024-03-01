83c750bc52f11a1ba880c7d14dba14137529ed37
emil4700 <emil.netz@charite.de>
2024-03-01 12:15:34 +0100

    added a script

---
+++ workflow/scripts/make_sample_tsv.sh
@@ -0,0 +1,20 @@
+#!/bin/bash 
+
+input_file=$1 
+output_file="files_tobe_analysed.tsv" 
+
+echo -e "samplename\tfq1\tfq2" > $output_file 
+
+while IFS= read -r file_name; do  
+    # get the sample name  
+    sample_name=${file_name%_S*} 
+ 
+    # get the filepaths 
+    fq1=$(find -name $sample_name*R1*)
+    fq2=$(find -name $sample_name*R2*)
+
+    # print values to tsv file 
+    echo -e "$sample_name\t$fq1\t$fq2" >> $output_file
+    echo "$sample_name added" 
+
+done < "$input_file"
