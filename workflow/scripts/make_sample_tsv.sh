174b96f1b2807672e921b8398e757f62267d9bea
emil4700 <emil.netz@charite.de>
2024-03-01 11:29:54 +0100

    new script

---
+++ workflow/scripts/make_sample_tsv.sh
@@ -0,0 +1,23 @@
+#!/bin/bash 
+
+dir=$1 
+output_file="resources/data/files.tsv" 
+
+echo -e "samplename\tfq1\tfq2" > $output_file 
+
+for fq in $dir/*; do 
+    # get the sample name 
+    file_name=$(basename $fq)
+    echo $file_name 
+    sample_name=${file_name%_S*} 
+    echo $sample_name
+ 
+    # get the filepaths 
+    fq1=$(find $dir -name $sample_name*R1*) 
+    echo $fq1 
+    fq2=$(find $dir -name $sample_name*R2*)
+
+    # print values to tsv file 
+    echo -e "$sample_name\t$fq1\t$fq2" >> $output_file
+
+done 
